9b2ccc1ac993cd80c6b62b30f6ee46b503288fa6
Andre Rousselet <andre098@hotmail.com>
2022-02-15 16:24:19 -0300

    Requisito 4: adiciona cor de fundo branca para cada div criada na função generatePixelBoard

--- script.js
+++ script.js
@@ -14,8 +14,9 @@ const pixelBoard = document.getElementById('pixel-board');
 
 function generatePixelBoard() {
   for (let index = 0; index < 25; index += 1) {
-    let pixel = document.createElement('div');
+    const pixel = document.createElement('div');
     pixel.classList.add('pixel');
+    pixel.style.background = 'white';
     pixelBoard.appendChild(pixel);
   }
 }
