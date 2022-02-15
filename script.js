0b4589aad6de09c5773737ca8c67222ec0b04ab9
Andre Rousselet <andre098@hotmail.com>
2022-02-15 15:43:39 -0300

    Cria função para gerar as cores em cada div como parâmetros, dinamizando requisitos 2 e 3

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
-const coresDaPaleta = ['black', 'darkblue', 'darkgreen', 'darkmagenta'];
 const divs = document.getElementsByClassName('color');
 
-for (let index = 0; index < 4; index += 1) {
-  divs[index].classList.add(coresDaPaleta[index]);
-  divs[index].style.background = coresDaPaleta[index];
+function generateColorOfDivs(color1, color2, color3, color4) {
+  const coresDaPaleta = [color1, color2, color3, color4];
+  for (let index = 0; index < 4; index += 1) {
+    divs[index].id = (coresDaPaleta[index]);
+    divs[index].style.background = coresDaPaleta[index];
+  }
 }
+
+generateColorOfDivs('black', 'darkblue', 'darkgreen', 'darkmagenta');
