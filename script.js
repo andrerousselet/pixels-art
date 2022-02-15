0573e1dce22bd99a50b7ab0b29cad9bc7fefe40c
Andre Rousselet <andre098@hotmail.com>
2022-02-15 19:03:18 -0300

    Requsito 8: cria variável setando a selectedColor ao carregar de preto, cria funçao getColor que modifica selectedColor para a cor clicada e adiciona modificação ao pixel clicado

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function generateColorsOfPalette(color1, color2, color3, color4) {
   const colorsOfPalette = [color1, color2, color3, color4];
   for (let index = 0; index < 4; index += 1) {
     colors[index].id = (colorsOfPalette[index]);
-    colors[index].style.background = colorsOfPalette[index];
+    colors[index].style.backgroundColor = colorsOfPalette[index];
   }
 }
 
@@ -31,6 +31,23 @@ function addClassSelected(event) {
   event.target.classList.add('selected');
 }
 
+let selectedColor = 'rgb(0 0 0)';
+
+function getColor(event) {
+  selectedColor = window.getComputedStyle(event.target, null).getPropertyValue('background-color');
+}
+
 for (let index = 0; index < colors.length; index += 1) {
   colors[index].addEventListener('click', addClassSelected);
+  colors[index].addEventListener('click', getColor);
+}
+
+const pixels = document.getElementsByClassName('pixel');
+
+function addColorToPixel(event) {
+  event.target.style.backgroundColor = selectedColor;
+}
+
+for (let index = 0; index < pixels.length; index += 1) {
+  pixels[index].addEventListener('click', addColorToPixel);
 }
